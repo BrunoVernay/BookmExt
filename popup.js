6c58433a2529d740cd2bd2d80a0c4d69ead4af34
BrunoVernay <BrunoVern.a@gmail.com>
2015-07-28 16:43:08 +0200

    Sample from https://developer.chrome.com/extensions/samples#my-bookmarks

--- popup.js
+++ popup.js
@@ -1,118 +1,128 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
+// Search the bookmarks when entering the search keyword.
+$(function() {
+  $('#search').change(function() {
+     $('#bookmarks').empty();
+     dumpBookmarks($('#search').val());
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+});
+// Traverse the bookmark tree, and print the folder and nodes.
+function dumpBookmarks(query) {
+  var bookmarkTreeNodes = chrome.bookmarks.getTree(
+    function(bookmarkTreeNodes) {
+      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
     });
-  });
+}
+function dumpTreeNodes(bookmarkNodes, query) {
+  var list = $('<ul>');
+  var i;
+  for (i = 0; i < bookmarkNodes.length; i++) {
+    list.append(dumpNode(bookmarkNodes[i], query));
+  }
+  return list;
+}
+function dumpNode(bookmarkNode, query) {
+  if (bookmarkNode.title) {
+    if (query && !bookmarkNode.children) {
+      if (String(bookmarkNode.title).indexOf(query) == -1) {
+        return $('<span></span>');
+      }
+    }
+    var anchor = $('<a>');
+    anchor.attr('href', bookmarkNode.url);
+    anchor.text(bookmarkNode.title);
+    /*
+     * When clicking on a bookmark in the extension, a new tab is fired with
+     * the bookmark url.
+     */
+    anchor.click(function() {
+      chrome.tabs.create({url: bookmarkNode.url});
+    });
+    var span = $('<span>');
+    var options = bookmarkNode.children ?
+      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
+      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
+        'href="#">Delete</a>]</span>');
+    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
+      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
+      '</td></tr></table>') : $('<input>');
+    // Show add and edit links when hover over.
+        span.hover(function() {
+        span.append(options);
+        $('#deletelink').click(function() {
+          $('#deletedialog').empty().dialog({
+                 autoOpen: false,
+                 title: 'Confirm Deletion',
+                 resizable: false,
+                 height: 140,
+                 modal: true,
+                 overlay: {
+                   backgroundColor: '#000',
+                   opacity: 0.5
+                 },
+                 buttons: {
+                   'Yes, Delete It!': function() {
+                      chrome.bookmarks.remove(String(bookmarkNode.id));
+                      span.parent().remove();
+                      $(this).dialog('destroy');
+                    },
+                    Cancel: function() {
+                      $(this).dialog('destroy');
+                    }
+                 }
+               }).dialog('open');
+         });
+        $('#addlink').click(function() {
+          $('#adddialog').empty().append(edit).dialog({autoOpen: false,
+            closeOnEscape: true, title: 'Add New Bookmark', modal: true,
+            buttons: {
+            'Add' : function() {
+               chrome.bookmarks.create({parentId: bookmarkNode.id,
+                 title: $('#title').val(), url: $('#url').val()});
+               $('#bookmarks').empty();
+               $(this).dialog('destroy');
+               window.dumpBookmarks();
+             },
+            'Cancel': function() {
+               $(this).dialog('destroy');
+            }
+          }}).dialog('open');
+        });
+        $('#editlink').click(function() {
+         edit.val(anchor.text());
+         $('#editdialog').empty().append(edit).dialog({autoOpen: false,
+           closeOnEscape: true, title: 'Edit Title', modal: true,
+           show: 'slide', buttons: {
+              'Save': function() {
+                 chrome.bookmarks.update(String(bookmarkNode.id), {
+                   title: edit.val()
+                 });
+                 anchor.text(edit.val());
+                 options.show();
+                 $(this).dialog('destroy');
+              },
+             'Cancel': function() {
+                 $(this).dialog('destroy');
+             }
+         }}).dialog('open');
+        });
+        options.fadeIn();
+      },
+      // unhover
+      function() {
+        options.remove();
+      }).append(anchor);
+  }
+  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
+  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
+    li.append(dumpTreeNodes(bookmarkNode.children, query));
+  }
+  return li;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  dumpBookmarks();
 });
